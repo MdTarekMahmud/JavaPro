a6ed48ba4a802e77d441a1cc96db3a4956907eb0
Md Tarek Mahmud <112857212+MdTarekMahmud@users.noreply.github.com>
2023-07-31 20:58:50 +0600

    Add files via upload

---
+++ JavaProject/src/App.java
@@ -0,0 +1,72 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App{
+    public static void main(String[] args){
+       // System.out.println("My first Java Program");
+        // String name = "Tarek";
+        // String neighbour = "Mahmud";
+        // String friend = neighbour;
+        // System.out.println(name);
+        // System.out.println(friend);
+
+        // byte age1 = 30;
+        // long phone = 1234567890L;
+        // float pi = 3.14F;
+        // boolean isAd = true;
+
+        // String name = new String("Tarek");// not compulsory
+        // System.out.println(name.length());
+
+
+        // --------------
+        // String name = "Tarek";
+        // System.out.println(name.charAt(3));
+        // name.replace('s','d') method
+
+        // arrays----------------------------------------
+    //     int [] marks = new int[3];
+    //     marks[0] = 98;
+    //     marks[1] = 95;
+    //     marks[2] =100;
+    //    // System.out.println(marks.length);
+    //     System.out.println(marks[0]);
+    //     Arrays.sort(marks);
+    //     System.out.println(marks[0]);
+
+            // int[][] resultMark = {{91,92,99},{95,96,97}};
+            // System.out.println(resultMark[0][2]);
+
+        //casting------------------------
+
+        //constant
+        //final float PI = 3.1416F;
+
+    //operators----------------------------------------
+        // int a =5,b=6;
+        // System.out.println(a>b);
+
+        // System.out.println(Math.max(4,5));
+        //System.out.println((int)(Math.random()*100));
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Input your age : ");
+        // int age  = sc.nextInt();
+        // System.out.println(age);
+
+
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your age : ");
+        // int age = sc.nextInt();
+        // if(age>=18)
+        //     System.out.println("Can");
+        // else    
+        //     System.out.println("Can't");
+
+        // for(int i=0;i<100;i++){
+        //     System.out.println(i);
+        // }
+
+    }
+}
